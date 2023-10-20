d9f39120a755bfd37a4798d39f48dad0125c7cc2
Mithil <mithilmudaliyar@gmail.com>
2023-10-20 09:56:08 +0530

    first

---
+++ pages/api/index.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const url =
+  "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw.lat,
+          tr_latitude: ne.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+        },
+        headers: {
+          'X-RapidAPI-Key': 'YOUR API GOES HERE',
+          'X-RapidAPI-Host': 'YOUR API HOST GOES HERE'
+        }
+      }
+    );
+    return data;
+  } catch (error) {
+    console.log(`Fetch data Error : ${error}`);
+  }
+};
